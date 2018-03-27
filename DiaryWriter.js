f45e1872b5d55dea34c29189de6e51bb4395cb72
bimuyu <bimuyu@helijia.com>
2018-03-27 14:35:25 +0800

    完善写日记界面

--- DiaryWriter.js
+++ DiaryWriter.js
@@ -15,15 +15,57 @@ let angryMood = require('./image/80.png');
 
 export default class DiaryWriter extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.diaryTitle = null;
+    this.diaryBody = null;
+    this.moodCode = 0;
+    this.state = {
+      moodText = '请选择心情'
+    };
+  }
+
   returnPressed() {
     Alert.alert(
       '请确认',
       '你确定要退回日记列表吗？',
       [
-        {text: '确定'},
+        {text: '确定', onPress: this.props.returnPressed },
         {text: '取消'}
       ]
     );
+  }
+
+  selectMood() {
+    let tempString;
+    if (this.moodCode === 5) {
+      this.moodCode = 1;
+    } else {
+      this.moodCode = this.moodCode + 1;
+    }
+    switch (this.moodCode) {
+      case 1:
+        tempString = '现在的心情：愤怒';
+        break;
+      case 1:
+        tempString = '现在的心情：愤怒';
+        break;
+      case 1:
+        tempString = '现在的心情：愤怒';
+        break;
+      case 1:
+        tempString = '现在的心情：愤怒';
+        break;
+      case 1:
+        tempString = '现在的心情：愤怒';
+        break;
+    }
+    this.setState( ()=>{
+      return {
+        moodText: tempString
+      };
+     });
   }
 
   render() {
