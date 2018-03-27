ef3d18236841f1a8c49bd04457bee5b56d97bbe1
bimuyu <bimuyu@helijia.com>
2018-03-27 14:28:57 +0800

    晚上读日记页面

--- DiaryReader.js
+++ DiaryReader.js
@@ -15,46 +15,42 @@ let angryMood = require('./image/80.png');
 
 export default class DiaryReader extends Component {
 
-  updateSearchKeyword(newWord) {
-
-  }
-
   render() {
     return (
       <View style={MCV.container}>
         <StatusBar hidden={false} />
         <View style={MCV.firstRow}>
-           <TouchableOpacity >
+           <TouchableOpacity onPress={this.props.returnPressed}>
               <Text style={MCV.middleButton}>
                 返回
               </Text>
            </TouchableOpacity>
-           <TouchableOpacity >
+           <TouchableOpacity onPress={this.props.readingPreviousPressed}>
               <Text style={MCV.middleButton}>
                 上一篇
               </Text>
            </TouchableOpacity>
-           <TouchableOpacity >
+           <TouchableOpacity onPress={this.props.readingNextPressed}>
               <Text style={MCV.middleButton}>
                 下一篇
               </Text>
            </TouchableOpacity>
         </View>
         <View style={MCV.secondRow}>
-          <Image style={MCV.moodStyle} source={angryMood} />
+          <Image style={MCV.moodStyle} source={this.props.diaryMood} />
           <View style={MCV.subViewInReader}>
             <TextInput style={MCV.textInReader} editable={false}>
-              日记标题：某变量  
+              {this.props.diaryTitle}
             </TextInput>
             <TextInput style={MCV.textInReader} editable={false}>
-              时间：某变量
+              {this.diaryTime}
             </TextInput>
           </View>
         </View>
         <TextInput style={[MCV.diaryBodyStyle, {color:'black'}]}
           multiline={true}
           editable={false}
-          value={'某变量记录日记正文'}
+          value={this.props.diaryBody}
         >
         </TextInput>
       </View>
